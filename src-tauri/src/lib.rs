e09ff100d766d053bf8f08109437b188d697ab48
Jakson-Almeida <jakson.almeida@engenharia.ufjf.br>
2024-08-05 15:58:27 -0300

    Update lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -104,7 +104,7 @@ pub fn log_error(tx: &SyncSender<Log>, msg: String) {
 
 // CSV files
 
-/// Aproxima um espectro de LPFG por uma lorentziana
+/// Aproxima um espectro de LPFG por uma lorentziana em Rust
 ///
 /// # Parâmetros
 ///
